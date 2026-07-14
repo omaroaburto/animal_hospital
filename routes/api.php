aaf165446b40398a507d096512b7df1dae806c87
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2026-07-13 23:57:04 -0400

    fix: actualización de rutas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\domains\Auth\Controllers\AdminController;
 use App\domains\Auth\Controllers\AuthController;
+use App\Domains\Auth\Controllers\PasswordResetController;
 use App\domains\Auth\Controllers\RoleController;
 use App\Domains\Clients\Controllers\ClientController;
 use App\Domains\Clients\Controllers\RegionController;
@@ -11,20 +12,22 @@ use Illuminate\Support\Facades\Route;
 /*************************************************************
  ***                   RUTAS PÚBLICAS                      ***
  *************************************************************/
-
-Route::post('v1/login',[AuthController::class,'login']);
-Route::post('refresh',[AuthController::class,'refresh']);
+Route::post('v1/auth/register',[ClientController::class, 'store']);
+Route::post('v1/auth/login',[AuthController::class,'login']);
+Route::post('v1/auth/refresh',[AuthController::class,'refresh']);
+Route::post('v1/auth/forgot-password',[AuthController::class,'forgotPassword']);
+Route::post('v1/auth/reset-password',[AuthController::class,'resetPassword'])
+    ->name('password.update');
+//Route::get('v1/reset-password', [PasswordResetController::class, 'create'])
+    //->name('password.reset');
 //Verificación de email (API)
 Route::get('v1/email/verify', [AuthController::class, 'verifyEmail']);
+//regiones y comunas
 Route::get('v1/regions', [RegionController::class, 'index']);
 Route::get('v1/regions/{region}', [RegionController::class, 'show']);
 Route::get('v1/regions/{region}/communes', [RegionController::class, 'showWithCommunes']);
-Route::post('v1/register',[ClientController::class, 'store']);
-Route::get('v1/clients',[ClientController::class, 'index']);
-Route::get('v1/clients/{client}',[ClientController::class, 'show']);
-Route::match(['put','patch'],'v1/clients/{client}',[ClientController::class, 'update']);
-Route::delete('v1/clients/{client}',[ClientController::class, 'destroy']);
-Route::post('v1/clients/restore/{client}',[ClientController::class, 'restore']);
+
+
 /*************************************************************
  ***      RUTAS CON JWT Y CUENTAS VERIFICADAS (email)      ***
  *************************************************************/
@@ -46,4 +49,11 @@ Route::middleware(['jwt.auth', 'verified:api'])->prefix('v1/')->group(function (
     Route::post('/admins/{admin}/restore', [AdminController::class, 'restore'])
     ->name('admins.restore');
     Route::post('logout',[AuthController::class,'logout']);
+
+    //CLIENTES
+    Route::get('v1/clients',[ClientController::class, 'index']);
+    Route::get('v1/clients/{client}',[ClientController::class, 'show']);
+    Route::match(['put','patch'],'v1/clients/{client}',[ClientController::class, 'update']);
+    Route::delete('v1/clients/{client}',[ClientController::class, 'destroy']);
+    Route::post('v1/clients/restore/{client}',[ClientController::class, 'restore']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Domains\Auth\Controllers\PasswordResetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reset-password', [PasswordResetController::class, 'create'])
+    ->name('password.reset');

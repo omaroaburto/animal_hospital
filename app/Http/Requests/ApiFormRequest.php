e678ad027485d3201654e7cf2989369b7f5d7434
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2026-07-15 19:59:10 -0400

    actualización de clase principal de form request, mensajes y atributos globales

--- app/Http/Requests/ApiFormRequest.php
+++ app/Http/Requests/ApiFormRequest.php
@@ -1,13 +1,17 @@
 <?php
 namespace App\Http\Requests;
 
-
+use App\Http\Requests\Concerns\HasValidationAttributes;
+use App\Http\Requests\Concerns\HasValidationMessages;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiFormRequest extends FormRequest{
+    use HasValidationMessages;
+    use HasValidationAttributes;
+    
     protected function failedValidation(Validator $validator){
         throw new HttpResponseException(response()->json([
             'message'=>'Error de validación.',
